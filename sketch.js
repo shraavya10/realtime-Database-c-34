3e44ef493564dff2a457e3412f6fddfcbb56bfa0
shraavya <deepthiherle@gmail.com>
2020-04-07 18:06:36 +0530

    realtime database

--- sketch.js
+++ sketch.js
@@ -1,29 +1,44 @@
 var ball;
-
+var database,position;
 function setup(){
     createCanvas(500,500);
+    database=firebase.database();
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var ballposition=database.ref("ball/position");
+    ballposition.on("value",readop);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writeposition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writeposition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writeposition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writeposition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writeposition(x,y){
+   // ball.x = ball.x + x;
+   // ball.y = ball.y + y;
+   database.ref("ball/position").set({
+       x:position.x+x,
+       y:position.y+y
+   })
+}
+
+function readop(data){
+position=data.val();
+ball.x=position.x;
+ball.y=position.y;
+
+
 }
